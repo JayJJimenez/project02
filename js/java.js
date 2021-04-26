0e9511e24f80f8acc95a0d0a5e32e06e85650b01
JonathanJimenez <jayjjimenez@gmail.com>
2021-04-25 23:25:07 -0400

    Completed Kanye Generator

--- js/java.js
+++ js/java.js
@@ -1,14 +1,9 @@
-const btn = document.getElementById("btn");
-
-
 const getQuote = () => {
     return fetch('https://api.kanye.rest')
     .then(data=>{return data.json()})
 }
 
 document.addEventListener('DOMContentLoaded', getQuote)
-
-btn.addEventListener("click", getQuote);
 
 getQuote().then(data => {
     const quoteDiv = makeQuoteDiv(data.quote)
@@ -19,6 +14,7 @@ const makeQuoteDiv = (quote => {
     console.log(quote)
     const div = document.createElement('div');
     const h3 = document.createElement('h3');
+ 
 
     h3.innerHTML = quote;
 
@@ -29,5 +25,16 @@ const makeQuoteDiv = (quote => {
 
 const appendQuote = (quoteDiv) => {
     const quoteContainer = document.getElementById("quotebox")
+    quoteContainer.innerHTML = ""
     quoteContainer.appendChild(quoteDiv)
 }
+
+
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", () => {
+    getQuote().then(data => {
+        const quoteDiv = makeQuoteDiv(data.quote)
+        appendQuote(quoteDiv)
+    })
+})
